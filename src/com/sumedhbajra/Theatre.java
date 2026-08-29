b919133ebdd48ace4966c746601d2c57f8829277
sumedhbajra <bajrasumedh9@gmail.com>
2026-08-29 14:43:43 +0545

    new changes, about comparable and comparator

--- src/com/sumedhbajra/Theatre.java
+++ src/com/sumedhbajra/Theatre.java
@@ -13,6 +13,19 @@ import java.util.*;
 public class Theatre {
     private final String theatreName;
     private List<Seat> seats = new ArrayList<>();
+
+    static final Comparator<Seat> PRICE_ORDER = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat seat1, Seat seat2) {
+            if(seat1.getPrice() < seat2.getPrice()) {
+                return -1;
+            } else if (seat1.getPrice() > seat2.getPrice()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
                                         //A-Z
     public Theatre(String theatreName, int numRows, int seatsPerRow) {
         this.theatreName = theatreName;
